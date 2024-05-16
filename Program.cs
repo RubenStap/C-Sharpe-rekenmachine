29d8926b9c342206b74bd859170440fe3b0143ac
RubenStap <120170289+RubenStap@users.noreply.github.com>
2024-05-16 17:15:07 +0200

    feature(rekenmachine)

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program
+{
+    public static int keer(int getala, int getalb)
+    {
+        return getala * getalb;
+    }
+    public static void Main()
+    {
+        Console.WriteLine("Getal a: ");
+        int getala = int.Parse(Console.ReadLine());
+        Console.WriteLine("Getal b: ");
+        int getalb = int.Parse(Console.ReadLine());
+
+        int resutaat = keer(getala, getalb);
+
+        Console.WriteLine("a x b = " + resutaat);
+
+    }
+
+
+
+}
